2bb6d223541a4cb1940f7f50f853467ffa36eddd
callumok2004 <callumokane123@gmail.com>
2021-11-06 12:30:00 +0000

    Updated

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,44 +8,25 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 import clientPromise from "../../../lib/database";
 
-export default NextAuth({
-    providers: [
-        Discord({
-            clientId: process.env.DISCORD_ID,
-            clientSecret: process.env.DISCORD_SECRET,
-            scope: "guilds"
-        })
-    ],
-    adapter: MongoDBAdapter({
-        db: async () => (await clientPromise).db("Ryft-Auth")
-    }),
-    // callbacks: {
-    //     jwt: async (token, user, account, profile, isNewUser) => {
-    //         if (profile) {
-    //             const guilds = await axios('https://discord.com/api/users/@me/guilds', {
-    //                 headers: {
-    //                     Authorization: `Bearer ${account.accessToken}`
-    //                 }
-    //             });
+export default async function auth(req, res) {
+    const db = await clientPromise;
 
-    //             await (await db('RyftSite', 'Users')).findOneAndUpdate(
-    //                 { userId: profile.id },
-    //                 { $set: { ...profile, guilds: guilds.data, account } },
-    //                 { upsert: true }
-    //             );
-    //         }
-    //         user && (token.user = user);
-    //         return Promise.resolve(token);
-    //     },
-    //     session: async (session, user, sessionToken) => {
-    //         const userr = await (await db('RyftSite', 'Users')).findOne({ userId: user.user.id });
-    //         session.user = userr || user.user;
-    //         return Promise.resolve(session);
-    //     },
-    //     redirect: async (url, baseUrl) => Promise.resolve('/dashboard')
-    // },
-    pages: {
-        signIn: "/dashboard",
-        error: "/auth/error"
-    }
-})
+    console.log(db.db)
+
+    return await NextAuth(req, res, {
+        providers: [
+            Discord({
+                clientId: process.env.DISCORD_ID,
+                clientSecret: process.env.DISCORD_SECRET,
+                scope: "guilds"
+            })
+        ],
+        adapter: MongoDBAdapter({
+            db: db.db("Ryft-Auth")
+        }),
+        pages: {
+            signIn: "/dashboard",
+            error: "/auth/error"
+        }
+    })
+}
--- src/pages/api/database.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const client = new MongoClient(process.env.MONGO_URI as string);
-
-export default async function database(dbName: string, collName: string) {
-    if (typeof dbName !== 'string') return (collName as any).json({ msg: 'no' });
-    await client.connect();
-    return client.db(dbName).collection(collName);
-}
