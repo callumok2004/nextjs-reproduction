1ed7ae6da5fe6d8611fd2ed18ab27a0289388b8e
callumok2004 <callumokane123@gmail.com>
2021-11-06 19:20:11 +0000

    Updated

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,32 +1,55 @@
-//@ts-nocheck
-
 import axios from "axios";
+import { Db, ObjectId } from "mongodb";
 import NextAuth from "next-auth";
-import Discord from "next-auth/providers/discord";
 
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 import clientPromise from "../../../lib/database";
 
 export default async function auth(req, res) {
-    const db = await clientPromise;
+  const db = await clientPromise,
+    instance: Db = db.db("Ryft-Auth")
 
-    console.log(db.db)
+  return await NextAuth(req, res, {
+    providers: [
+      {
+        id: "discord",
+        name: "Discord",
+        type: "oauth",
+        authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
+        token: "https://discord.com/api/oauth2/token",
+        userinfo: "https://discord.com/api/users/@me",
+        async profile(profile, tokens) {
+          if (profile.avatar === null) {
+            const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+            profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
+          } else {
+            const format = profile.avatar.startsWith("a_") ? "gif" : "png"
+            profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`
+          }
 
-    return await NextAuth(req, res, {
-        providers: [
-            Discord({
-                clientId: process.env.DISCORD_ID,
-                clientSecret: process.env.DISCORD_SECRET,
-                scope: "guilds"
-            })
-        ],
-        adapter: MongoDBAdapter({
-            db: db.db("Ryft-Auth")
-        }),
-        pages: {
-            signIn: "/dashboard",
-            error: "/auth/error"
-        }
+          const guilds = await axios('https://discord.com/api/users/@me/guilds', {
+            headers: {
+              Authorization: `Bearer ${tokens.access_token}`
+            }
+          });
+
+          return {
+            ...profile,
+            guilds
+          };
+        },
+        clientId: process.env.DISCORD_ID,
+        clientSecret: process.env.DISCORD_SECRET
+      }
+    ],
+    pages: {
+      signIn: "/dashboard",
+      error: "/auth/error"
+    },
+    adapter: MongoDBAdapter({
+      db: () => instance,
+      ObjectId
     })
+  })
 }
